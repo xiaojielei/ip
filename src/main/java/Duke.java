020ee957afa25991f9c232ddea48749f58a0d71e
Xiao Jielei <142099098+xiaojielei@users.noreply.github.com>
2025-01-28 21:49:49 +0800

    .

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
+        String greeting = "Hello! I'm XiaoJielei\n" + "What can I do for you?";
+        System.out.println(greeting);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String exiting =  "Bye. Hope to see you again soon!";
+        System.out.println(exiting);
     }
 }
