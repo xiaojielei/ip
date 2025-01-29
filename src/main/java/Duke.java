8c102f2dcc67bd413e953f7ee21f9a6231032bd0
Xiao Jielei <142099098+xiaojielei@users.noreply.github.com>
2025-01-30 00:09:37 +0800

    Level - 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,14 +16,26 @@ public class Duke {
                 System.out.println(exiting);
                 break;
             } else if (input.contains("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; tasks[i] != null; i++) {
-                    System.out.println(i+1 + ". " + tasks[i]);
+                    System.out.println(i+1 + "." + tasks[i]);
                 }
+            } else if (!input.contains("unmark") && input.contains("mark")) {
+                String index = input.substring(5);
+                String replaced = tasks[Integer.parseInt(index) - 1].replace("[ ]", "[X]");
+                tasks[Integer.parseInt(index) - 1] = replaced;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" "  +replaced);
+            } else if (input.contains("unmark")) {
+                String index = input.substring(7);
+                String replaced = tasks[Integer.parseInt(index) - 1].replace("[X]", "[ ]");
+                tasks[Integer.parseInt(index) - 1] = replaced;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(" " + replaced);
             } else {
-                tasks[taskCount++] = input;
+                tasks[taskCount++] = "[ ] " + input;
                 System.out.println("added: " + input);
             }
-
         }
 
     }
