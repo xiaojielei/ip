9f8b2488c1b4eac92314fa97cdcf0771336ca124
Xiao Jielei <142099098+xiaojielei@users.noreply.github.com>
2025-01-29 17:16:56 +0800

    Level - 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         String greeting = "Hello! I'm Shew\n" + "What can I do for you?";
         System.out.println(greeting);
@@ -13,9 +15,15 @@ public class Duke {
                 String exiting = "Bye. Hope to see you again soon!";
                 System.out.println(exiting);
                 break;
+            } else if (input.contains("list")) {
+                for (int i = 0; tasks[i] != null; i++) {
+                    System.out.println(i+1 + ". " + tasks[i]);
+                }
             } else {
-                System.out.println("    " + input);
+                tasks[taskCount++] = input;
+                System.out.println("added: " + input);
             }
+
         }
 
     }
